d32c18cf0f55fe2930da9dc878b703e35cab619a
axelkirken <axel.kierkegaard@outlook.com>
2022-06-30 16:24:48 +0200

    started building roboapp

--- src/index.js
+++ src/index.js
@@ -1,13 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './App';
+import CardList from "./Cardlist";
 import reportWebVitals from './reportWebVitals';
+import "tachyons";
+import {robots} from "./robots"
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <div>
+      <CardList robots={robots}/>
+    </div>
+    
   </React.StrictMode>
 );
 
